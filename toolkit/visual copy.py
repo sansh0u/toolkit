518248586637fbbc401a9865f5b53fa28b31be18
sansh0u <1952730137@qq.com>
2026-04-16 13:41:47 +0800

    4.16

---
+++ toolkit/visual copy.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+df = pd.read_csv(
+    "/mnt/d/download/HYS_sample1_sorted.bed.gz",
+    sep="\t",
+    header=None,
+    names=["chr", "start", "end", "name", "score"]
+)
+df["length"] = df["end"] - df["start"]
+import matplotlib.pyplot as plt
+
+
+def plot_fragment_distribution(df, out):
+    plt.figure()
+    plt.hist(df["length"], bins=100)
+    plt.xlabel("Fragment size")
+    plt.ylabel("Count")
+    plt.title("Fragment Size Distribution")
+    plt.savefig(out)
+    plt.close()
+def plot_chr_distribution(df, out):
+    counts = df["chr"].value_counts()
+
+    plt.figure()
+    counts.plot(kind="bar")
+    plt.xlabel("Chromosome")
+    plt.ylabel("Reads")
+    plt.title("Chromosome Distribution")
+    plt.savefig(out)
+    plt.close()
+def plot_coverage(df, out):
+    df["mid"] = (df["start"] + df["end"]) // 2
+
+    plt.figure()
+    plt.hist(df["mid"], bins=200)
+    plt.xlabel("Genome position")
+    plt.ylabel("Reads")
+    plt.title("Coverage Distribution")
+    plt.savefig(out)
+    plt.close()
+
+plot_fragment_distribution(df, "fragment_distribution.png")
+plot_chr_distribution(df, "chr_distribution.png")
+plot_coverage(df, "coverage_distribution.png")
--- toolkit/visual.py
+++ toolkit/visual.py
@@ -1,1 +1,21 @@
-import snapatac2
+import snapatac2 as snap
+
+
+
+data_path = "/mnt/d/download/HYS_sample1_sorted.bed.gz"
+adata = snap.pp.read_fragment(
+    data_path
+)
+
+
+#qc_plot(data)
+snap.pp.calculate_qc_metrics(adata)
+snap.pl.qc(adata)
+
+
+
+def qc_plot(data):
+    '''
+    绘制质量控制图
+    '''
+    
